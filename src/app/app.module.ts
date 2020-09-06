824a293d55a2e94a68212b6f1c6f7e9dc6a48ce1
Filipe Cabral <filipecabraal@gmail.com>
2020-09-05 22:47:02 -0300

    Implementation of methods to load and delete categories

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { HttpClientInMemoryWebApiModule } from 'angular-in-memory-web-api';
 import { InMemoryDataBase } from './in-memory-database';
@@ -13,8 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase)
+    HttpClientModule,
+    HttpClientInMemoryWebApiModule.forRoot(InMemoryDataBase),
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
